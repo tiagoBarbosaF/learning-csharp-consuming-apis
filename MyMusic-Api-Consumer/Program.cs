633b25af1418c9aa44ee5fba227f184e9d1b86a7
Tiago Barbosa <tiago@farias.cc>
2023-07-01 15:18:22 -0300

    initial commit.

---
+++ MyMusic-Api-Consumer/Program.cs
@@ -0,0 +1,16 @@
+﻿// using var client = new HttpClient();
+// try
+// {
+//     var response = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
+//     Console.WriteLine(response);
+// }
+// catch (Exception e)
+// {
+//     Console.WriteLine($"Exception: {e.Message}");
+// }
+
+using System.Net.Http.Json;
+
+using var client = new HttpClient();
+var response = await client.GetStringAsync("https://pokeapi.co/api/v2/pokemon/bulbasaur");
+Console.WriteLine(response);
